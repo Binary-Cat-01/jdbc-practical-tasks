6568a8714f3312d7624a604e38eab04989a6bebd
Binary-Cat-01 <kotpivowar@gmail.com>
2025-03-06 15:45:05 +0300

    feat: add to passengerRepository methods for find passengers by id, male, birthdate

--- src/main/java/com/walking/jdbc/repository/PassengerRepository.java
+++ src/main/java/com/walking/jdbc/repository/PassengerRepository.java
@@ -4,6 +4,7 @@ import com.walking.jdbc.mapper.PassengerMapper;
 import com.walking.jdbc.model.Passenger;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 
 public class PassengerRepository {
@@ -13,7 +14,7 @@ public class PassengerRepository {
     public PassengerRepository(PassengerMapper mapper) {
         this.mapper = mapper;
     }
-    
+
     public void createPassengerTableIfNotExists() {
         String sql = """
                     create table if not exists passenger (
@@ -67,25 +68,6 @@ public class PassengerRepository {
         }
     }
 
-//    Реализация уязвимая для SQL-инъекций:
-//    public List<Passenger> findByFullName(String firstName, String lastName) {
-//        try (Connection connection = getConnection();
-//             Statement statement = connection.createStatement()) {
-//
-//            String sql = "select * from passenger where first_name = %s and last_name = %s"
-//                    .formatted(
-//                            "'" + firstName + "'",
-//                            "'" + lastName + "'"
-//                    );
-//
-//            ResultSet result = statement.executeQuery(sql);
-//
-//            return mapper.map(result);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
     public List<Passenger> findAll() {
         String sql = """
                 select * from passenger
@@ -94,11 +76,59 @@ public class PassengerRepository {
         try (Connection connection = getConnection();
              Statement statement = connection.createStatement()) {
 
-            ResultSet rs = statement.executeQuery(sql);
+            ResultSet result = statement.executeQuery(sql);
 
-            return mapper.map(rs);
+            return mapper.map(result);
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка при получении пассажиров", e);
+        }
+    }
+
+    public List<Passenger> findById(Long id) {
+        String sql = "select from passenger where id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1, id);
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            return mapper.map(result);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка при получении пассажира по id", e);
+        }
+    }
+
+    public List<Passenger> findByMale(Boolean male) {
+        String sql = "select from passenger where male = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setBoolean(1, male);
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            return mapper.map(result);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка при получении пассажиров по полу", e);
+        }
+    }
+    
+    public  List<Passenger> findByBirthDate (LocalDate birthDate) {
+        String sql = "select from passenger where birth_date = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setDate(1, Date.valueOf(birthDate));
+
+            ResultSet result = preparedStatement.executeQuery();
+
+            return mapper.map(result);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка при получении пассажиров по дате рождения", e);
         }
     }
 
