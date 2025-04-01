15ecd6d6173db9ca155f851b52291f7e6fb81673
Binary-Cat-01 <kotpivowar@gmail.com>
2025-04-01 23:32:38 +0300

    feat: create ticket entity

---
+++ src/main/java/com/walking/jdbc/model/Ticket.java
@@ -0,0 +1,43 @@
+package com.walking.jdbc.model;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+    private Long id;
+    private Long passengerId;
+    private Long flightId;
+
+    private LocalDateTime purchaseDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(Long passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
