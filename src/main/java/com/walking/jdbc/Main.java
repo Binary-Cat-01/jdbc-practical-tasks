6e0c42bc016adb00d1a53fd4fb94747520b0f8be
Binary-Cat-01 <kotpivowar@gmail.com>
2025-04-02 15:45:54 +0300

    feat: add flyway migration code in psvm

--- src/main/java/com/walking/jdbc/Main.java
+++ src/main/java/com/walking/jdbc/Main.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.flywaydb.core.Flyway;
 
 public class Main {
     private final static Logger log = LogManager.getLogger(Main.class);
@@ -11,5 +12,11 @@ public class Main {
     public static void main(String[] args) {
         var config = new HikariConfig("/src/main/resources/hikari.properties");
         var dataSource = new HikariDataSource(config);
+
+        Flyway.configure()
+              .dataSource(dataSource)
+              .baselineOnMigrate(true)
+              .load()
+              .migrate();
     }
 }
