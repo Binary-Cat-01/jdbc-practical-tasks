2e6d8883d6faffb712ffcdb5c3a30b0504df7093
Binary-Cat-01 <kotpivowar@gmail.com>
2025-04-02 14:20:41 +0300

    feat: create a dataSource object in psvm to use HicariCP

--- src/main/java/com/walking/jdbc/Main.java
+++ src/main/java/com/walking/jdbc/Main.java
@@ -1,10 +1,15 @@
 package com.walking.jdbc;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
     private final static Logger log = LogManager.getLogger(Main.class);
 
-
+    public static void main(String[] args) {
+        var config = new HikariConfig("/src/main/resources/hikari.properties");
+        var dataSource = new HikariDataSource(config);
+    }
 }
