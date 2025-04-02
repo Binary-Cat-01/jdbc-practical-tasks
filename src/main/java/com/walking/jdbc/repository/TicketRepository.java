104a773a8ef8cc5faf0ff175b511c6d9599586db
Binary-Cat-01 <kotpivowar@gmail.com>
2025-04-02 18:14:33 +0300

    feat: add api for TicketRepository

--- src/main/java/com/walking/jdbc/repository/TicketRepository.java
+++ src/main/java/com/walking/jdbc/repository/TicketRepository.java
@@ -1,10 +1,13 @@
 package com.walking.jdbc.repository;
 
 import com.walking.jdbc.mapper.TicketMapper;
+import com.walking.jdbc.model.Ticket;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
 
 public class TicketRepository {
     private final TicketMapper mapper;
@@ -13,5 +16,45 @@ public class TicketRepository {
     public TicketRepository(TicketMapper mapper, DataSource dataSource) {
         this.mapper = mapper;
         this.dataSource = dataSource;
+    }
+
+    public List<Ticket> findAll() {
+
+    }
+
+    public List<Ticket> findById(Long id) {
+
+    }
+
+    public List<Ticket> findByPassengerId(Long id) {
+
+    }
+
+    public List<Ticket> findByFlightId(Long id) {
+
+    }
+
+    public Ticket add(Ticket ticket) {
+
+    }
+
+    public void addAll(List<Ticket> tickets) {
+
+    }
+
+    public Ticket update(Ticket ticket) {
+
+    }
+
+    public void updateAll(List<Ticket> tickets) {
+
+    }
+
+    public Ticket delete(Ticket ticket) {
+
+    }
+
+    public void deleteAll(List<Ticket> tickets) {
+
     }
 }
