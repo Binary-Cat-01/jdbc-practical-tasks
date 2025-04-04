2c8c081f235622769ff7721913c5f661e939b3a1
Binary-Cat-01 <kotpivowar@gmail.com>
2025-04-04 22:07:39 +0300

    feat: create PassengerService class with changePurchaseDate method

---
+++ src/main/java/com/walking/jdbc/service/PassengerService.java
@@ -0,0 +1,20 @@
+package com.walking.jdbc.service;
+
+import com.walking.jdbc.model.Passenger;
+import com.walking.jdbc.repository.PassengerRepository;
+
+import java.time.LocalDateTime;
+
+public class PassengerService {
+    private final PassengerRepository passengerRepository;
+
+    public PassengerService(PassengerRepository passengerRepository) {
+        this.passengerRepository = passengerRepository;
+    }
+
+    public Passenger changePurchaseDate(Passenger passenger, LocalDateTime time) {
+        passenger.setLastPurchase(time);
+
+        return passenger;
+    }
+}
